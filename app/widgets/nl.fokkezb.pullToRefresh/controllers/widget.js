df89e8548a4c57c27ad70eca6e026eb96974f3f9
Fokke Zandbergen <mail@fokkezandbergen.nl>
2012-11-22 16:25:24 +0100

    Fully redesigned API (nog in docs yet)

--- app/widgets/nl.fokkezb.pullToRefresh/controllers/widget.js
+++ app/widgets/nl.fokkezb.pullToRefresh/controllers/widget.js
@@ -1,54 +1,151 @@
 var args = arguments[0] || {};
 
-var options = _.defaults(args, {
-	msgPull: 'Pull down to refresh...',
-	msgRelease: 'Release to refresh...',
-	msgUpdating: 'Updating...',
-	msgUpdated: 'Last Updated: %s %s'
-});
+var options = null;
 
+var initted = false;
 var pulling = false;
-var reloading = false;
+var pulled = false;
+var loading = false;
+
 var offset = 0;
 
-function doReset() {
-    reloading = false;
-    var date = new Date();
-    $.updated.text = String.format(options.msgUpdated, String.formatDate(date, 'short'), String.formatTime(date, 'short'));
-    $.activityIndicator.hide();
-    $.arrow.transform=Ti.UI.create2DMatrix();
-    $.arrow.show();
-    $.status.text = options.msgPull;
-    args.table.setContentInsets({top:0}, {animated:true});
+function doShow(msg) {
+	
+	if (pulled) {
+		return false;
+	}
+	
+	pulled = true;
+	
+    $.status.text = msg || options.msgUpdating;
+    $.arrow.hide();
+    $.activityIndicator.show();
+    
+    options.table.setContentInsets({top:80}, {animated:true});
+    
+    return true;
 }
 
-args.table.setHeaderPullView($.headerPullView);
- 
-args.table.addEventListener('scroll',function(e){
+function doHide() {
+	
+	if (!pulled) {
+		return false;
+	}
+    
+    options.table.setContentInsets({top:0}, {animated:true});
+    
+    $.activityIndicator.hide();
+    $.arrow.transform = Ti.UI.create2DMatrix();
+    $.arrow.show();
+    $.status.text = options.msgPull;
+    
+   	pulled = false;
+}
+
+function setDate(date) {
+	
+	if (date === false) {
+		$.updated.hide();
+	
+	} else {
+		$.updated.show();
+	
+		if (date !== true) {
+			$.updated.text = String.format(options.msgUpdated, String.formatDate(date, 'short'), String.formatTime(date, 'short'));
+		}
+	}
+}
+
+function finishLoading(success) {
+
+    if (success) {
+   		setDate(new Date());
+   	}
+    
+    doHide();
+    
+    loading = false;
+}
+
+function scrollListener(e) {
     offset = e.contentOffset.y;
-    if (pulling && !reloading && offset > -80 && offset < 0){
+    
+    if (pulled) {
+    	return;
+    }
+    
+    if (pulling && !loading && offset > -80 && offset < 0){
         pulling = false;
         var unrotate = Ti.UI.create2DMatrix();
         $.arrow.animate({transform:unrotate, duration:180});
         $.status.text = options.msgPull;
-    } else if (!pulling && !reloading && offset < -80){
+        
+    } else if (!pulling && !loading && offset < -80){
         pulling = true;
         var rotate = Ti.UI.create2DMatrix().rotate(180);
         $.arrow.animate({transform:rotate, duration:180});
         $.status.text = options.msgRelease;
     }
-});
- 
-args.table.addEventListener('dragEnd', function (e){
-    if (pulling && !reloading && offset < -80){
-        pulling = false;
-        reloading = true;
-        $.status.text = options.msgUpdating;
-        $.arrow.hide();
-        $.activityIndicator.show();
-        e.source.setContentInsets({top:80}, {animated:true});
-        args.load();
-    }
-});
+}
 
-exports.reset = doReset;
+function dragEndListener(e) {
+	
+    if (!pulled && pulling && !loading && offset < -80){
+        pulling = false;
+        loading = true;
+       
+		doShow();
+
+        options.loader(finishLoading);
+    }
+}
+
+function doInit(args) {
+	
+	if (initted) {
+		return false;
+	}
+
+	options = _.defaults(args, {
+		msgPull: 'Pull down to refresh...',
+		msgRelease: 'Release to refresh...',
+		msgUpdating: 'Updating...',
+		msgUpdated: 'Last Updated: %s %s'
+	});
+
+	options.table.setHeaderPullView($.headerPullView);
+	 
+	options.table.addEventListener('scroll', scrollListener);
+	options.table.addEventListener('dragEnd', dragEndListener);
+}
+
+function doRemove() {
+
+	if (!initted) {
+		return false;
+	}
+
+	options.table.setHeaderPullView(null);
+	
+	options.table.removeEventListener('scroll', scrollListener);
+	options.table.removeEventListener('dragEnd', dragEndListener);
+	
+	options = null;
+	
+	initted = false;
+	pulling = false;
+	loading = false;
+	shown = false;
+	
+	offset = 0;
+}
+
+if (args.table && args.loader) {
+	doInit(args);
+}
+
+exports.init = doInit;
+exports.show = doShow;
+exports.hide = doHide;
+exports.date = setDate;
+exports.remove = doRemove;

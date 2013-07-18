a1a5248ca72121a8692bd822c4e529a5f004cd59
Fokke Zandbergen <mail@fokkezb.nl>
2013-07-18 12:16:45 +0200

    Major rewrite bringing - Retina arrow images - Grey (default) arrow images - Removed date text for more clean view - Renamed all classes for easy widget styling

--- controllers/widget.js
+++ controllers/widget.js
@@ -10,129 +10,120 @@ var loading = false;
 var offset = 0;
 
 function doShow(msg) {
-	
+
 	if (!initted || pulled) {
 		return false;
 	}
-	
+
 	pulled = true;
-	
-    $.status.text = msg || options.msgUpdating;
-    $.arrow.hide();
-    $.activityIndicator.show();
-    
-    options.table.setContentInsets({top:80}, {animated:true});
-    
-    return true;
+
+	$.ptrText.text = msg || options.msgUpdating;
+	$.ptrArrow.hide();
+	$.ptrIndicator.show();
+
+	options.table.setContentInsets({
+		top : options.offset
+	}, {
+		animated : true
+	});
+
+	return true;
 }
 
 function doHide() {
-	
+
 	if (!initted || !pulled) {
 		return false;
 	}
-    
-    options.table.setContentInsets({top:0}, {animated:true});
-    
-    $.activityIndicator.hide();
-    $.arrow.transform = Ti.UI.create2DMatrix();
-    $.arrow.show();
-    $.status.text = options.msgPull;
-    
-   	pulled = false;
-}
 
-function setDate(date) {
-	
-	if (!initted) {
-		return false;
-	}
-	
-	if (date === false) {
-		$.updated.hide();
-	
-	} else {
-		$.updated.show();
-	
-		if (date !== true) {
-			$.updated.text = String.format(options.msgUpdated, String.formatDate(date, 'short'), String.formatTime(date, 'short'));
-		}
-	}
+	options.table.setContentInsets({
+		top : 0
+	}, {
+		animated : true
+	});
+
+	$.ptrIndicator.hide();
+	$.ptrArrow.transform = Ti.UI.create2DMatrix();
+	$.ptrArrow.show();
+	$.ptrText.text = options.msgPull;
+
+	pulled = false;
 }
 
 function doTrigger() {
-	
+
 	if (!initted || loading) {
 		return false;
 	}
-	
+
 	loading = true;
-	
+
 	doShow();
-	
+
 	options.loader(finishLoading);
 }
 
-function finishLoading(success) {
+function finishLoading() {
+	doHide();
 
-    if (success) {
-   		setDate(new Date());
-   	}
-    
-    doHide();
-    
-    loading = false;
+	loading = false;
 }
 
 function scrollListener(e) {
-    offset = e.contentOffset.y;
-    
-    if (pulled) {
-    	return;
-    }
-    
-    if (pulling && !loading && offset > -80 && offset < 0){
-        pulling = false;
-        var unrotate = Ti.UI.create2DMatrix();
-        $.arrow.animate({transform:unrotate, duration:180});
-        $.status.text = options.msgPull;
-        
-    } else if (!pulling && !loading && offset < -80){
-        pulling = true;
-        var rotate = Ti.UI.create2DMatrix().rotate(180);
-        $.arrow.animate({transform:rotate, duration:180});
-        $.status.text = options.msgRelease;
-    }
+	offset = e.contentOffset.y;
+
+	if (pulled) {
+		return;
+	}
+
+	if (pulling && !loading && offset > -options.offset && offset < 0) {
+		pulling = false;
+		var unrotate = Ti.UI.create2DMatrix();
+		$.ptrArrow.animate({
+			transform : unrotate,
+			duration : 180
+		});
+		$.ptrText.text = options.msgPull;
+
+	} else if (!pulling && !loading && offset < -options.offset) {
+		pulling = true;
+		var rotate = Ti.UI.create2DMatrix().rotate(180);
+		$.ptrArrow.animate({
+			transform : rotate,
+			duration : 180
+		});
+		$.ptrText.text = options.msgRelease;
+	}
 }
 
 function dragEndListener(e) {
-	
-    if (!pulled && pulling && !loading && offset < -80){
-        pulling = false;
-       
+
+	if (!pulled && pulling && !loading && offset < -options.offset) {
+		pulling = false;
+
 		doTrigger();
-    }
+	}
 }
 
 function doInit(args) {
-	
+
 	if (initted || !OS_IOS) {
 		return false;
 	}
-	
+
 	initted = true;
 
 	options = _.defaults(args, {
-		msgPull: 'Pull down to refresh...',
-		msgRelease: 'Release to refresh...',
-		msgUpdating: 'Updating...',
-		msgUpdated: 'Last Updated: %s %s'
+		msgPull : L('ptrPull', 'Pull to refresh...'),
+		msgRelease : L('ptrRelease', 'Release to refresh...'),
+		msgUpdating : L('ptrUpating', 'Updating...'),
+		offset : 50
 	});
-	
-	$.status.text = options.msgPull;
 
-	options.table.setHeaderPullView($.headerPullView);
-	 
+	$.ptrText.text = options.msgPull;
+
+	options.table.setHeaderPullView($.ptr);
+
 	options.table.addEventListener('scroll', scrollListener);
 	options.table.addEventListener('dragEnd', dragEndListener);
 }
@@ -144,17 +135,17 @@ function doRemove() {
 	}
 
 	options.table.setHeaderPullView(null);
-	
+
 	options.table.removeEventListener('scroll', scrollListener);
 	options.table.removeEventListener('dragEnd', dragEndListener);
-	
+
 	options = null;
-	
+
 	initted = false;
 	pulling = false;
 	loading = false;
 	shown = false;
-	
+
 	offset = 0;
 }
 
@@ -162,9 +153,8 @@ if (args.table && args.loader) {
 	doInit(args);
 }
 
-exports.init    = doInit;
-exports.show    = doShow;
-exports.hide    = doHide;
-exports.date    = setDate;
+exports.init = doInit;
+exports.show = doShow;
+exports.hide = doHide;
 exports.trigger = doTrigger;
-exports.remove  = doRemove;
+exports.remove = doRemove;
